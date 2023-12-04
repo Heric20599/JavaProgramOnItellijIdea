8e87fef21dc145c1534264d45251ad40add3f7de
Heric20599 <141494309+Heric20599@users.noreply.github.com>
2023-12-04 18:15:22 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,63 @@
+package DSABOOTCAMP.RECURSION.INTRO;
+
+public class Number {
+        public static void main(String[] args) {
+            // write a function that takes in a number and prints it
+            // print first 5 numbers: 1 2 3 4 5
+
+            print1(1);
+        }
+
+//        static void print1(int n) {
+//            print2(2);
+//            System.out.println(n);
+//
+//        }
+//
+//        static void print2(int n) {
+//            print3(3);
+//            System.out.println(n);
+//
+//        }
+//
+//        static void print3(int n) {
+//            print4(4);
+//            System.out.println(n);
+//
+//        }
+//
+//        static void print4(int n) {
+//            print5(5);
+//            System.out.println(n);
+//
+//        }
+//
+//        // func body here changes
+//        static void print5(int n) {
+//            System.out.println(n);
+//        }
+    static void print1(int n) {
+            System.out.println(n);
+            print2(2);
+        }
+
+        static void print2(int n) {
+            System.out.println(n);
+            print3(3);
+        }
+
+        static void print3(int n) {
+            System.out.println(n);
+            print4(4);
+        }
+
+        static void print4(int n) {
+            System.out.println(n);
+            print5(5);
+        }
+
+        // func body here changes
+        static void print5(int n) {
+            System.out.println(n);
+        }
+}
