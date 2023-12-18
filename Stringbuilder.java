bb244a4500595421eaf387cd728529754d180029
Heric20599 <141494309+Heric20599@users.noreply.github.com>
2023-12-18 22:07:34 +0530

    Add files via upload

---
+++ Stringbuilder.java
@@ -0,0 +1,17 @@
+package DSABOOTCAMP.STRING.STRINGBUILDER;
+
+public class Stringbuilder {
+        public static void main(String[] args) {
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                char ch = (char)('a' + i);
+                builder.append(ch);
+            }
+
+            System.out.println(builder.toString());
+
+            builder.reverse();
+
+            System.out.println(builder);
+        }
+}
